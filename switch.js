155447b19a68a2c87cc694ece9fc07de7b1b22c3
EcaCosca <enriquecoscarelli@gmail.com>
2023-11-24 15:57:19 +0100

    Done with switch

---
+++ switch.js
@@ -0,0 +1,17 @@
+// https://www.w3schools.com/js/js_switch.asp
+
+let color = "yellow";
+
+switch(color) {
+    case "red":
+      console.log("Stop!")
+      break;
+      case "yellow":
+        console.log("Careful!")
+      break;
+      case "green":
+        console.log("Go!")
+      break;
+    default:
+      console.log("You might be drunk")
+  }
