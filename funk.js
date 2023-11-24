a18c4e0b6c509314e08df32db3174ebdeda5becb
EcaCosca <enriquecoscarelli@gmail.com>
2023-11-24 16:38:22 +0100

    Done with all the funk

--- funk.js
+++ funk.js
@@ -9,16 +9,41 @@
 // funk()
 // funk()
 
+const school = "WBS"
 
-function courseGreeter(student){
-    console.log("Welcome to the course " + student)
+function courseGreeter(student = "student", batch = 46){
+    const school = "Enrique's school"
+    console.log("Welcome to the course " + student+` your batch is number ${batch} in ${school}`)
 }
 
-courseGreeter("Tania")
-courseGreeter("Subaru")
-courseGreeter("Nestor")
-courseGreeter("Antonio")
-courseGreeter("Lena")
-courseGreeter("Mustafa")
-courseGreeter("Mr.Jones")
-courseGreeter()
+// courseGreeter("Tania", 30)
+// courseGreeter("Subaru", 46)
+// courseGreeter("Nestor")
+// courseGreeter("Antonio")
+// courseGreeter("Lena")
+// courseGreeter("Mustafa")
+// courseGreeter("Mr.Jones")
+// courseGreeter()
+
+// console.log(school)
+
+
+
+function dynamite(){
+    console.log("BOOM!")
+}
+
+const bomb = () => console.log("BOOM!")
+
+// dynamite()
+// bomb()
+
+const adder = (num1, num2) => num1 + num2
+
+console.log(adder(5, 10))
+
+const multuplyBy2 = num1 => num1*2
+
+const result = multuplyBy2(50)
+
+console.log(result)
