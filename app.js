a4079df807d942b7501f302e3c6aae8cca2448e3
EcaCosca <enriquecoscarelli@gmail.com>
2023-11-24 15:49:34 +0100

    Co-authored-by: Subaru Goto <Subaru-Goto@users.noreply.github.com> Co-authored-by: Lena <AnyLena@users.noreply.github.com> Co-authored-by: Néstor Iriondo <nestoririondo@users.noreply.github.com> Co-authored-by: Christina Vekri <islandarber@users.noreply.github.com> Co-authored-by: TeeAtlas <TeeAtlas@users.noreply.github.com>

---
+++ app.js
@@ -0,0 +1,55 @@
+// CREATE A VARIABLE THAT YOU CAN CHANGE WITH YOUR NAME IN IT
+
+let name123 = "Lena"
+
+let itsMe = "Nestor"
+
+let myName = "Antonio";
+
+let name = 'Subaru'
+ 
+let cjName = 'Ciaran';
+ 
+let age = 22;
+
+age = 11;
+
+if(age > 18){
+    console.log('You are an adult')
+}else if(age == 18){
+    console.log('You are 18, welcome to being an adult')
+}else{
+    console.log('You are too young, get out of here')
+}
+
+if(myName == "Antonio"){
+    console.log("Are you Anotnio Banderas?")
+    if(myName == "Antonio Banderas"){
+        console.log("Welcome Antonio Banderas")
+    }else{
+        console.log("You are not Zorro")
+    }
+}
+
+// TERNARY OPERATOR 
+
+myName == "Antonio" ?
+
+    myName == "Antonio Banderas" ? console.log("Welcome Antonio Banderas!!!"):console.log("You are not Zorro!!!")
+
+    : console.log("You are not Antonio!!!")
+
+
+if(age === "11"){
+    console.log("this is 11")
+}else{
+    console.log("this is not 11")
+}
+
+// TERNARY OPERATOR 
+age !== "11" ? console.log("this is 11") : console.log("this is not 11");
+
+
+
+
+
