e69e52cf2250905c807cc2b263c8d4342973fc38
EcaCosca <enriquecoscarelli@gmail.com>
2023-11-24 16:15:03 +0100

    Done with arrays and objects

---
+++ array.js
@@ -0,0 +1,45 @@
+const name = "Messi"
+
+let class46 = [
+"Enrique",
+"Christina",
+'Subaru',
+"Antonio",
+'tania',
+'Ciaran',
+"Lena",
+"Nestor",
+'',
+true,
+false,
+null,
+'Subaru, coolests car in history',
+123,
+name,
+[1,2,3,4,5,6,7,8,9,10,"bingo"],
+]
+
+
+
+
+const student = {
+    name: "Subaru",
+    age: 22,
+    isCool: true,
+    favoriteFood: "pizza",
+    favoriteDrink: "water",
+    favoriteColor: "blue",
+    favoriteCar: "Subaru",
+    favoriteMovies: "The Dark Knight",
+    address: {
+        street: "123 main st",
+        city: "Miami",
+        state: "FL"
+    },
+}
+
+console.log(student.name)
+console.log(student.age)
+console.log(student.isCool)
+console.log(student['favoriteFood'])
+console.log(student.address.city)
