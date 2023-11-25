db921fbb35fe9da1deab17a04613083ab19506f3
EcaCosca <enriquecoscarelli@gmail.com>
2023-11-25 13:28:05 +0100

    Done

---
+++ dataTypes.js
@@ -0,0 +1,47 @@
+// STRING
+
+var user1 = "NO MORE VAR"
+
+let user2 = 'Subaru'
+const user3 = "Alejandro"
+let user4 = `Antonio`
+
+
+const team1 = user2+" & "+user3
+const welcomeMessage = `Hello team 1! Welcome ${team1}`
+
+console.log(welcomeMessage)
+// console.log(user1)
+// console.log(user2)
+// console.log(user3)
+// console.log(user4)
+
+// user3 = "Lena"
+
+// console.log(user3)
+
+// BOOLEAN
+
+let boolean = true
+let otherBoolean = false
+
+// console.log(typeof boolean)
+// console.log(typeof otherBoolean)
+// console.log(typeof user1)
+
+// INT or numbers
+
+const num = 1
+let num1 = -2
+let num3 = 3.5
+let num4 = 0
+let num4point5 = 1/2
+let Num4 = "12"
+
+
+// console.log(typeof num)
+// console.log(typeof num1)
+// console.log(typeof num3)
+// console.log(typeof num4)
+// console.log(typeof Num4)
+
